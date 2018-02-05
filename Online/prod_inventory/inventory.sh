7c2f36e58fccd24c6563804c6b1833e94e483a0a
S.nake <412369794@qq.com>
2018-02-05 10:57:20 +0800

    check study

---
+++ Online/prod_inventory/inventory.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+export ANSIBLE_CONFIG=/Users/snake/Develop/code/AutoOps/ansible.cfg
+export ZABBIX_USER=Admin
+export ZABBIX_URL=https://monitor.dragonest.com/api_jsonrpc.php
+export ZABBIX_PASSWORD=
+
+a=`/Users/snake/Develop/code/AutoOps/Online/prod_inventory/inventory -g "Group EN Doom-game"`
+
+echo $a
